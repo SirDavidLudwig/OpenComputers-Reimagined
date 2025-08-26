484839ae0e3062c57fbf0a06c3ddfc9a242d5337
David Ludwig <davidludwigii@gmail.com>
2025-08-26 09:18:04 -0500

    Added ALU, PCB, and Raw PCB with recipes

--- common/src/main/java/tech/dlii/opencomputers/item/Items.java
+++ common/src/main/java/tech/dlii/opencomputers/item/Items.java
@@ -21,12 +21,18 @@ public class Items {
     public static final RegistrySupplier<Item> CASE = ITEMS.register(Blocks.CASE.getId().getPath(), () -> new BlockItem(Blocks.CASE.get(), baseProperties(Blocks.CASE.getId().getPath())));
 
     // Items
+    public static final RegistrySupplier<Item> ALU = register("alu", () -> new Item(baseProperties("alu")));
+    public static final RegistrySupplier<Item> DIAMOND_NUGGET = register("diamond_nugget", () -> new Item(baseProperties("diamond_nugget")));
+
     public static final RegistrySupplier<Item> MEMORY1 = register("memory1", () -> new Item(baseProperties("memory1")));
     public static final RegistrySupplier<Item> MEMORY2 = register("memory2", () -> new Item(baseProperties("memory2")));
     public static final RegistrySupplier<Item> MEMORY3 = register("memory3", () -> new Item(baseProperties("memory3")));
     public static final RegistrySupplier<Item> MEMORY4 = register("memory4", () -> new Item(baseProperties("memory4")));
     public static final RegistrySupplier<Item> MEMORY5 = register("memory5", () -> new Item(baseProperties("memory5")));
     public static final RegistrySupplier<Item> MEMORY6 = register("memory6", () -> new Item(baseProperties("memory6")));
+
+    public static final RegistrySupplier<Item> PRINTED_CIRCUIT_BOARD = register("printed_circuit_board", () -> new Item(baseProperties("printed_circuit_board")));
+    public static final RegistrySupplier<Item> RAW_CIRCUIT_BOARD = register("raw_circuit_board", () -> new Item(baseProperties("raw_circuit_board")));
 
     public static void initialize() {
         ITEMS.register();
