c41eed9240c9326ef82b49bc509373036592216b
Shazuli <sigge-e@hotmail.com>
2025-09-03 22:03:15 +0200

    W.I.P. Got shift-click componenets out of a case but not into one.

--- common/src/main/java/tech/dlii/opencomputers/common/inventory/CaseMenu.java
+++ common/src/main/java/tech/dlii/opencomputers/common/inventory/CaseMenu.java
@@ -8,8 +8,10 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
 import tech.dlii.opencomputers.api.Tier;
 import tech.dlii.opencomputers.api.driver.item.SlotType;
+import tech.dlii.opencomputers.common.item.MemoryItem;
 
 import java.util.List;
 
@@ -21,6 +23,111 @@ public class CaseMenu extends AbstractBaseContainerMenu {
     public static CaseMenu create(int containerId, Inventory playerInventory, final FriendlyByteBuf data) {
         final int tier = data.readInt();
         return new CaseMenu(containerId, playerInventory, new SimpleContainer(InventorySlots.CASE.get(tier).size()), new SimpleContainerData(1), tier);
+    }
+
+    /*@Override
+    public ItemStack quickMoveStack(Player player, int fromIndex)
+    {
+        ItemStack newStack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(fromIndex);
+
+        if (slot.hasItem()) {
+
+            ItemStack oriStack = slot.getItem();
+            newStack = oriStack.copy();
+
+
+
+            if (fromIndex < this.playerInventory.getContainerSize()) {
+
+                if (!this.moveItemStackTo(oriStack, this.playerInventory.getContainerSize(), this.slots.size(), true))
+                    return ItemStack.EMPTY;
+
+            } else if (!this.moveItemStackTo(oriStack, 0, this.playerInventory.getContainerSize(), false)) {
+
+                return ItemStack.EMPTY;
+            }
+
+            if (oriStack.isEmpty())
+                slot.set(ItemStack.EMPTY);
+            else
+                slot.setChanged();
+
+        }
+
+        return newStack;
+    }*/
+
+    /*@Override
+    public ItemStack quickMoveStack(Player player, int fromIndex) {
+
+        ItemStack newStack = ItemStack.EMPTY;// Start with an empty stack
+        Slot slot = this.slots.get(fromIndex);
+
+        if (slot.hasItem()) {
+
+            ItemStack oriStack = slot.getItem();
+            newStack = oriStack.copy();
+
+            InventorySlots inventorySlots = InventorySlots.CASE.get(tier);
+            InventorySlots.InventorySlot componentSlot = inventorySlots.get(fromIndex);
+
+            if (fromIndex < this.playerInventory.getContainerSize()) {
+                if (!this.moveItemStackTo(oriStack, fromIndex, componentSlot.index(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.moveItemStackTo(oriStack, componentSlot.index(), 0, false)) {
+
+                return ItemStack.EMPTY;
+            }
+
+            // Set it to empty if transferred the whole stack or mark it as changed.
+            if (oriStack.isEmpty())
+                slot.set(ItemStack.EMPTY);
+            else
+                slot.setChanged();
+        }
+
+
+        return newStack;
+        //return super.quickMoveStack(player, i);
+    }*/
+
+    @Override
+    public ItemStack quickMoveStack(Player player, int fromIndex) {
+
+        //ItemStack newStack = ItemStack.EMPTY;// Start with an empty stack
+        Slot slot = this.slots.get(fromIndex);
+
+        if (slot.hasItem()) {
+
+            ItemStack oriStack = slot.getItem();
+
+            if (slot instanceof ComponentSlot) {// Computer -> Player inv
+
+                if (!this.moveItemStackTo(oriStack, fromIndex, this.playerInventory.getContainerSize(), true))
+                    return ItemStack.EMPTY;
+                slot.set(ItemStack.EMPTY);
+
+            } else {// Player inv -> Computer
+
+                /*ItemStack newStack = oriStack.copy();
+
+                InventorySlots inventorySlots = InventorySlots.CASE.get(tier);
+
+
+
+
+                // Set it to empty if transferred the whole stack or mark it as changed.
+                if (oriStack.isEmpty())
+                    slot.set(ItemStack.EMPTY);
+                else
+                    slot.setChanged();
+
+                return newStack;*/
+            }
+        }
+        return ItemStack.EMPTY;
     }
 
     // Server-side Constructor
@@ -74,6 +181,13 @@ public class CaseMenu extends AbstractBaseContainerMenu {
             this.addSlot(new ComponentSlot(container, slot.index(), 142, 16 + (i+inventorySlots.get(SlotType.HDD).size())*18, slot.type(), slot.tier()));
         }
 
+        bindInventory(playerInventory);
+
+        this.addDataSlots(containerData);
+    }
+
+    protected void bindInventory(Inventory playerInventory)
+    {
         // Add player inventory slots
         for (int row = 0; row < 3; ++row) {
             for (int col = 0; col < 9; ++col) {
@@ -84,9 +198,8 @@ public class CaseMenu extends AbstractBaseContainerMenu {
         for (int col = 0; col < 9; ++col) {
             this.addSlot(new Slot(playerInventory, col, 8 + col * 18, 142));
         }
-
-        this.addDataSlots(containerData);
     }
+
 
     @Override
     public boolean stillValid(Player player) {
