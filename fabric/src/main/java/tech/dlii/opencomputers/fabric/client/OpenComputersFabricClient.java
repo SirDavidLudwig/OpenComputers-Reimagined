a9b8c1adccc608eea1e3965f858c697cc160c006
David Ludwig <davidludwigii@gmail.com>
2025-09-04 16:21:15 -0500

    Move case screen registration to Fabric client

--- fabric/src/main/java/tech/dlii/opencomputers/fabric/client/OpenComputersFabricClient.java
+++ fabric/src/main/java/tech/dlii/opencomputers/fabric/client/OpenComputersFabricClient.java
@@ -1,14 +1,17 @@
 package tech.dlii.opencomputers.fabric.client;
 
+import dev.architectury.event.events.client.ClientLifecycleEvent;
 import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry;
 import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
+import dev.architectury.registry.menu.MenuRegistry;
 import net.fabricmc.api.ClientModInitializer;
-import tech.dlii.opencomputers.OpenComputers;
+import tech.dlii.opencomputers.client.gui.screen.CaseScreen;
 import tech.dlii.opencomputers.client.renderer.blockentity.ScreenBlockEntityRenderer;
 import tech.dlii.opencomputers.client.renderer.color.ScreenBlockColor;
 import tech.dlii.opencomputers.common.block.Blocks;
 import tech.dlii.opencomputers.client.renderer.color.CaseBlockColor;
 import tech.dlii.opencomputers.common.block.entity.BlockEntityTypes;
+import tech.dlii.opencomputers.common.inventory.MenuTypes;
 
 public final class OpenComputersFabricClient implements ClientModInitializer {
     @Override
@@ -18,5 +21,9 @@ public final class OpenComputersFabricClient implements ClientModInitializer {
         ColorHandlerRegistry.registerBlockColors(new ScreenBlockColor(), Blocks.SCREEN_TIER_1.get(), Blocks.SCREEN_TIER_2.get(), Blocks.SCREEN_TIER_3.get());
 
         BlockEntityRendererRegistry.register(BlockEntityTypes.SCREEN.get(), ScreenBlockEntityRenderer::new);
+
+        ClientLifecycleEvent.CLIENT_STARTED.register(client -> {
+            MenuRegistry.registerScreenFactory(MenuTypes.CASE.get(), CaseScreen::new);
+        });
     }
 }
